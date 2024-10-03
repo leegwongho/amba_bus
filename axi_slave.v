49eac53452999b0df0c3c49a4a3b366ce91aa227
pdh1111111111111111111111 <ehdehddl980416@naver.com>
2024-10-03 23:31:12 +0900

    Update axi_slave.v

--- axi_slave.v
+++ axi_slave.v
@@ -2,180 +2,186 @@ module axi_slave(
     // clk, reset
     input ACLK,
     input ARESET,
+        
+    // AW(address write) CHANNEL
+    input AWVALID,
+    input [31:0] AWADDR,
+    output reg AWREADY,
 
-    // aw(address write) channel
-    input      [31:0]   AWADDR,
-    input               AWVALID,
-    output reg          AWREADY,
+    // W(write) CHANNEL
+    input WVALID,
+    input [31:0] WDATA,
+    input [3:0] WSTRB,
+    output reg WREADY,
 
-    // write channel
-    input      [31:0]   WDATA,
-    input               WVALID,
-    input      [3:0]    WSTRB,
-    output reg          WREADY,
-
-    // response Channel
-    input               BREADY,
-    output reg          BRESP,
-    output reg          BVALID);
-
-    reg [7:0] slave_memory[0:15];
-
-    // AW channel //
-    reg [31:0] aw_addr_reg, aw_addr_reg_next;
-
-    parameter AW_IDLE = 2'b01;
-    parameter AW_READY = 2'b10;
-
-    reg [1:0] aw_state, aw_state_next;
-
-    always @ (posedge ACLK , negedge ARESET) begin
-        if(!ARESET) begin
-            aw_state = AW_IDLE;
-            aw_addr_reg = 0;
-        end
-        else begin
-            aw_state = aw_state_next;
-            aw_addr_reg = aw_addr_reg_next;
-        end
-    end
-
-    always @ (posedge ACLK) begin
-        if(!ARESET) begin
-            aw_state_next = AW_IDLE;
+    // B CHANNEL (write response)
+    input BREADY,
+    output reg BVALID,
+    output reg [1:0] BRESP,
+    
+    // AR(address read) channel
+    input ARVALID,
+    input [31:0] ARADDR,
+    output reg ARREADY,
+    
+    // R channel (read channel)
+    input RREADY,
+    output reg RVALID,
+    output reg [31:0] RDATA,
+    output reg [1:0] RRESP,
+    
+    // user control master bit
+    input valid,
+    input [31:0] aw_addr,
+    input [31:0] w_data,
+    input [3:0] w_strb,
+    input read_valid,
+    output reg ready
+);    
+    
+    reg [31:0] memory [0:255];
+    
+    // AW(address write)CHANNEL
+    reg aw_state;
+    
+    parameter AW_IDLE = 1'b0;
+    parameter AW_VALID = 1'b1;
+    
+    always @(posedge ACLK or negedge ARESET) begin
+        if (!ARESET) begin
             AWREADY = 1'b0;
-        end
+            aw_state = AW_IDLE;
+        end 
         else begin
-            case (aw_state) 
+            case (aw_state)
                 AW_IDLE: begin
-                    AWREADY = 1'b0;
-                    if (AWVALID) begin // valid 신호 수신
-                        aw_state_next = AW_READY;
-                    end
-                end 
-                AW_READY : begin
                     AWREADY = 1'b1;
-                    aw_addr_reg_next = AWADDR;
-                    if (AWVALID && AWREADY) begin
-                        aw_state_next = AW_IDLE;
+                    if (AWVALID) begin
+                        AWREADY = 1'b0;
+                        aw_state = AW_VALID;
                     end
                 end
-                default: aw_state_next = AW_IDLE;
+                AW_VALID: begin
+                    if (BREADY) begin
+                        aw_state = AW_IDLE;
+                    end
+                end
             endcase
         end
     end
 
-    // write channel //
+    // W(write)CHANNEL
+    reg w_state;
+    
+    parameter W_IDLE = 1'b0;
+    parameter W_VALID = 1'b1;
 
-    reg [31:0] write_data_reg, write_data_reg_next;
-    reg [3:0] w_strb_reg, w_strb_reg_next;
-
-    parameter WRITE_IDLE = 2'b01;
-    parameter WRITE_READY = 2'b10;
-
-    reg [1:0] write_state, write_state_next;
-
-    always @ (posedge ACLK , negedge ARESET) begin
-        if(!ARESET) begin
-            write_state = WRITE_IDLE;
-            write_data_reg = 0;
-            w_strb_reg = 0;
-        end
-        else begin
-            write_state = write_state_next;
-            write_data_reg = write_data_reg_next;
-            w_strb_reg = w_strb_reg_next;
-        end
-    end
-
-    always @ (posedge ACLK) begin
-        if(!ARESET) begin
-            write_state_next = WRITE_IDLE;
+    always @(posedge ACLK or negedge ARESET) begin
+        if (!ARESET) begin
             WREADY = 1'b0;
-        end
+            w_state = W_IDLE;
+        end 
         else begin
-            write_data_reg_next = write_data_reg;
-            w_strb_reg_next = w_strb_reg;
-            case (write_state) 
-                WRITE_IDLE: begin
-                    WREADY = 1'b0;
-                    if (AWREADY && WVALID) begin // 주소를 입력 받았고 valid 신호를 받았을때
-                        write_state_next = WRITE_READY;
-                        write_data_reg_next = WDATA;
-                        w_strb_reg_next = WSTRB;
-                    end
-                end 
-                WRITE_READY : begin
+            case (w_state)
+                W_IDLE: begin
                     WREADY = 1'b1;
-                    if (w_strb_reg[0]) begin
-                        slave_memory[aw_addr_reg] = write_data_reg[7:0];
-                    end
-                    if (w_strb_reg[1]) begin
-                        slave_memory[aw_addr_reg + 1] = write_data_reg[15:8];
-                    end
-                    if (w_strb_reg[2]) begin
-                        slave_memory[aw_addr_reg + 2] = write_data_reg[23:16];
-                    end
-                    if (w_strb_reg[3]) begin
-                        slave_memory[aw_addr_reg + 3] = write_data_reg[31:24];
-                    end
-                    
-                    if (WVALID && WREADY) begin
-                        write_state_next = WRITE_IDLE;
+                    if (WVALID) begin
+                        if (valid) begin
+                            if (WSTRB[0]) memory[aw_addr[7:0]] = w_data;  
+                            if (WSTRB[1]) memory[aw_addr[7:0] + 1] = w_data;  
+                            if (WSTRB[2]) memory[aw_addr[7:0] + 2] = w_data;  
+                            if (WSTRB[3]) memory[aw_addr[7:0] + 3] = w_data;  
+                        end
+                        WREADY = 1'b0;  
+                        w_state = W_VALID;
                     end
                 end
-                default: write_state_next = WRITE_IDLE;
+                W_VALID: begin
+                    if (BREADY) begin
+                        w_state = W_IDLE;  
+                    end
+                end
             endcase
         end
     end
-
-    // response channel //
-
-    reg [31:0] response_reg, response_reg_next;
-
-    parameter RESPONSE_IDLE = 2'b01;
-    parameter RESPONSE_VALID = 2'b10;
-
-    reg [1:0] response_state, response_state_next;
-
-    always @ (posedge ACLK , negedge ARESET) begin
-        if(!ARESET) begin
-            response_state = RESPONSE_IDLE;
-            response_reg = 0;
-        end
-        else begin
-            response_state = response_state_next;
-            response_reg = response_reg_next;
-        end
-    end
-
-    always @ (posedge ACLK) begin
-        if(!ARESET) begin
-            response_state_next = RESPONSE_IDLE;
-            response_reg = 0;
+    
+    // B CHANNEL (write response)
+    always @(posedge ACLK or negedge ARESET) begin
+        if (!ARESET) begin
             BVALID = 1'b0;
-        end
+            BRESP = 2'b00;  
+        end 
         else begin
-            response_reg_next = response_reg;
-            case (response_state) 
-                RESPONSE_IDLE: begin
-                    BVALID = 1'b0;
-                    if (WVALID && WREADY) begin // write 데이터가 처리되면 response 출력
-                        response_state_next = RESPONSE_VALID;
-                        response_reg_next = 2'b00; // okay 
-                    end
-                end 
-                RESPONSE_VALID : begin
-                    BVALID = 1'b1;
-                    BRESP = response_reg;
-                    if (BVALID && BREADY) begin
-                        response_state_next = RESPONSE_IDLE;
+            if (WREADY && WVALID) begin
+                BVALID = 1'b1;  
+                BRESP = 2'b00;  
+            end 
+            else if (BREADY && BVALID) begin
+                BVALID = 1'b0; 
+            end
+        end
+    end
+    
+    // AW(address read)CHANNEL
+    reg ar_state;
+    
+    parameter AR_IDLE = 1'b0;
+    parameter AR_VALID = 1'b1;
+
+    always @(posedge ACLK or negedge ARESET) begin
+        if (!ARESET) begin
+            ARREADY = 1'b0;
+            ar_state = AR_IDLE;
+        end else begin
+            case (ar_state)
+                AR_IDLE: begin
+                    ARREADY = 1'b1;
+                    if (ARVALID) begin
+                        ARREADY = 1'b0;
+                        ar_state = AR_VALID;
                     end
                 end
-                default:;
+                AR_VALID: begin
+                    if (RREADY) begin
+                        ar_state = AR_IDLE;
+                    end
+                end
             endcase
+        end
+    end
+    
+    // R(read)CHANNEL
+    always @(posedge ACLK or negedge ARESET) begin
+        if (!ARESET) begin
+            RVALID = 1'b0;
+            RDATA = 32'b0;
+            RRESP = 2'b00; 
+        end 
+        else begin
+            if (ARREADY && ARVALID) begin
+                RVALID = 1'b1;  
+                RDATA = memory[ARADDR[7:0]];  
+                RRESP = 2'b00; 
+            end 
+            else if (RREADY && RVALID) begin
+                RVALID = 1'b0;  
+            end
+        end
+    end
+    
+    always @(posedge ACLK or negedge ARESET) begin
+        if (!ARESET) begin
+            ready = 1'b0;
+        end 
+        else begin
+            ready = 1'b0;  
+            if (WREADY && WVALID) begin
+                ready = 1'b1;  
+            end
+            if (ARREADY && ARVALID) begin
+                ready = 1'b1;  
+            end
         end
     end
 
 endmodule
-
