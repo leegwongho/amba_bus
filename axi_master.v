7e4271ea3ccba3b81a20f7e04d2fc3c01f38470b
pdh1111111111111111111111 <ehdehddl980416@naver.com>
2024-10-03 23:29:12 +0900

    Update axi_master.v

--- axi_master.v
+++ axi_master.v
@@ -1,30 +1,44 @@
 module axi_master(
     // clk, reset 
-    input               ACLK,
-    input               ARESET,
+    input ACLK,
+    input ARESET,
     
-    // AW(address write)CHANNEL
-    input               AWREADY,
-    output  reg         AWVALID,
-    output  reg [31:0]  AWADDR,
+    // AW(address write) CHANNEL
+    input AWREADY,
+    output reg AWVALID,
+    output reg [31:0] AWADDR,
 
-    // W(write)CHANNEL
-    input               WREADY,
-    output  reg         WVALID,
-    output  reg [31:0]  WDATA,
-    output  reg [3:0]   WSTRB, 
+    // W(write) CHANNEL
+    input WREADY,
+    output reg WVALID,
+    output reg [31:0] WDATA,
+    output reg [3:0] WSTRB, 
 
     // B CHANNEL (write response)
-    input       [1:0]   BRESP,
-    input               BVALID,
-    output  reg         BREADY,
+    input [1:0] BRESP,
+    input BVALID,
+    output reg BREADY,
     
+    // AR(address read) CHANNEL
+    output reg ARVALID,
+    output reg [31:0] ARADDR,
+    input ARREADY,
+
+    // R (read) CHANNEL
+    input RVALID,
+    input [31:0] RDATA,
+    input [1:0] RRESP,
+    output reg RREADY,
+
     // user control master bit
-    input               valid,
-    input        [31:0] aw_addr,
-    input        [31:0] w_data,
-    input        [3:0]  w_strb,
-    output  reg         ready);
+    input valid,
+    input read_valid, 
+    input [31:0] aw_addr,
+    input [31:0] w_data,
+    input [3:0]  w_strb,
+    input [31:0] ar_addr,    
+    output reg ready
+);
 
     // aw channel //
     reg [31:0] aw_addr_reg, aw_addr_reg_next;
@@ -56,14 +70,14 @@ module axi_master(
             aw_addr_reg_next = aw_addr_reg;
             case (aw_state) 
                 AW_IDLE : begin
-                    AWVALID = 1'b0; // 데이터 전송을 하지않는것을 slave 에게 알리는 비트
-                    if (valid) begin // cpu -> axi 한테 데이터를 요청
+                    AWVALID = 1'b0; 
+                    if (valid) begin 
                         aw_state_next = AW_VALID;
-                        aw_addr_reg_next = aw_addr;   // cpu가 보낸 주소 
+                        aw_addr_reg_next = aw_addr;  
                     end                
                 end
                 AW_VALID : begin
-                    AWVALID = 1'b1; // 데이터 전송중임을 slave 에게 알리는 비트
+                    AWVALID = 1'b1; 
                     AWADDR = aw_addr_reg;
                     if (AWVALID && AWREADY) begin
                         aw_state_next = AW_IDLE;
@@ -106,8 +120,8 @@ module axi_master(
             w_data_reg_next = w_data_reg;
             case (w_state)
                 W_IDLE :  begin
-                    WVALID = 1'b0; // 데이터 전송중임을 slave 에게 알리는 비트 
-                    if (valid) begin // cpu -> axi 한테 쓰기를 요청
+                    WVALID = 1'b0;
+                    if (valid) begin
                         w_state_next = W_VALID;
                         w_data_reg_next = w_data;
                         w_strb_reg_next = w_strb;
@@ -127,7 +141,6 @@ module axi_master(
     end
 
     // write response //
-
     reg [31:0] response_reg, response_reg_next;
 
     parameter RESP_IDLE = 2'b01;
@@ -158,19 +171,101 @@ module axi_master(
             case (resp_state)
                 RESP_IDLE :  begin
                     BREADY = 1'b0;
-                    if (WVALID) begin // write data 가 전송되면 다음 state로 변경
+                    if (WVALID) begin
                         resp_state_next = RESP_READY;
                     end
                 end
                 RESP_READY : begin
-                    BREADY = 1'b1; // ready비트가 먼저 1로 유지하다 valid 신호가 들어오면 전송완료 처리
+                    BREADY = 1'b1;
                     if (BVALID && BREADY) begin
                         resp_state_next = RESP_IDLE;
                         response_reg_next = BRESP;
-                        ready = 1'b1; // 작성 트랜잭션이 완료되면 ready 외부로 출력
+                        ready = 1'b1;
                     end
                 end
                 default: ;
+            endcase
+        end
+    end
+
+    // read address channel //
+    reg [31:0] ar_addr_reg, ar_addr_reg_next;
+    
+    parameter AR_IDLE = 2'b01;
+    parameter AR_VALID = 2'b10;
+    
+    reg [1:0] ar_state, ar_state_next;
+    
+    always @(posedge ACLK , negedge ARESET) begin
+        if (!ARESET) begin
+            ar_state = AR_IDLE;
+            ar_addr_reg = 0;
+        end 
+        else begin
+            ar_state = ar_state_next;
+            ar_addr_reg = ar_addr_reg_next;
+        end 
+    end
+    
+    always @ (posedge ACLK) begin
+        if (!ARESET) begin
+            ar_state_next = AR_IDLE;
+            ARVALID = 1'b0;
+            ar_addr_reg_next = 0;
+        end
+        else begin
+            ARVALID = 1'b0;
+            ar_addr_reg_next = ar_addr_reg;
+            case (ar_state) 
+                AR_IDLE : begin
+                    ARVALID = 1'b0; 
+                    if (read_valid) begin 
+                        ar_state_next = AR_VALID;
+                        ar_addr_reg_next = ar_addr;  
+                    end                
+                end
+                AR_VALID : begin
+                    ARVALID = 1'b1; 
+                    ARADDR = ar_addr_reg;
+                    if (ARREADY && ARVALID) begin
+                        ar_state_next = AR_IDLE;
+                    end
+                end
+                default: ar_state_next = AR_IDLE;
+            endcase
+        end
+    end
+
+    // read data channel //
+    reg [31:0] r_data_reg;
+    
+    parameter R_IDLE = 2'b01;
+    parameter R_VALID = 2'b10;
+    
+    reg [1:0] r_state, r_state_next;
+    
+    always @(posedge ACLK, negedge ARESET) begin
+        if (!ARESET) begin
+            r_state = R_IDLE;
+            RREADY = 1'b0;
+        end
+        else begin
+            r_state = r_state_next;
+            case (r_state)
+                R_IDLE : begin
+                    RREADY = 1'b0;
+                    if (ARVALID && ARREADY) begin 
+                        r_state_next = R_VALID;
+                    end
+                end
+                R_VALID : begin
+                    RREADY = 1'b1; 
+                    if (RVALID && RREADY) begin
+                        r_state_next = R_IDLE;
+                        r_data_reg = RDATA;
+                    end
+                end
+                default: r_state_next = R_IDLE;
             endcase
         end
     end
