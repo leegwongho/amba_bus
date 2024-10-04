d6f4febd8da3141f5a2084c000a346cb3a1d689b
pdh1111111111111111111111 <ehdehddl980416@naver.com>
2024-10-04 14:43:45 +0900

    Update tb_AXI.v

--- tb_AXI.v
+++ tb_AXI.v
@@ -1,151 +1,136 @@
+module tb_axi_master_slave();
 
-module tb_AXI_Memory ();
+// clk, reset(active low)
+    reg ACLK;
+    reg ARESET;
 
-    // Global Signal
-    reg        ACLK;
-    reg        ARESET;
+    // axi master
+    reg valid, read_valid;
+    reg [31:0] aw_addr, w_data, ar_addr;
+    reg [3:0] w_strb;
+    wire AWVALID, WVALID, BREADY, ARVALID, RREADY;
+    wire [31:0] AWADDR, WDATA, ARADDR;
+    wire [3:0] WSTRB;
+    wire [1:0] BRESP, RRESP;
+    wire BVALID, RVALID;
+    
+    // axi slave
+    wire AWREADY, WREADY, ARREADY;
+    wire [31:0] RDATA;
+    
+    axi_master2 master (
+        .ACLK(ACLK),
+        .ARESET(ARESET),
+        .AWREADY(AWREADY),
+        .AWVALID(AWVALID),
+        .AWADDR(AWADDR),
+        .WREADY(WREADY),
+        .WVALID(WVALID),
+        .WDATA(WDATA),
+        .WSTRB(WSTRB),
+        .BRESP(BRESP),
+        .BVALID(BVALID),
+        .BREADY(BREADY),
+        .ARVALID(ARVALID),
+        .ARADDR(ARADDR),
+        .ARREADY(ARREADY),
+        .RVALID(RVALID),
+        .RDATA(RDATA),
+        .RRESP(RRESP),
+        .RREADY(RREADY),
+        .valid(valid),
+        .read_valid(read_valid),
+        .aw_addr(aw_addr),
+        .w_data(w_data),
+        .w_strb(w_strb),
+        .ar_addr(ar_addr),    
+        .ready()
+    );
 
-    // AW Channel
-    wire [31:0]        AWADDR;
-    wire        AWVALID;
-    wire        AWREADY;
-
-    // W Channel
-    wire [31:0]        WDATA;
-    wire        WVALID;
-    wire [3:0]        WSTRB;
-    wire        WREADY;
-
-    // B Channel
-    wire        BRESP;
-    wire        BVALID;
-    wire        BREADY;
-
-    // external signal
-    integer        aw_addr;
-    integer        w_data;
-    reg        valid;
-    reg [3:0]        w_strb;
-    wire        ready;
-
-    // AXI_Master dut_master (.*);
-    // AXI_Slave_Memory dut_slave (.*);
-
-
-
-
-
-
-
-
-    axi_master DUT_MASTER(  ACLK,
-                            ARESET,
-                            AWREADY,
-                            AWVALID,
-                            AWADDR,
-                            WREADY,
-                            WVALID,
-                            WDATA,
-                            WSTRB, 
-                            BRESP,
-                            BVALID,
-                            BREADY,
-                            valid,
-                            aw_addr,
-                            w_data,
-                            w_strb,
-                            ready);
-
-
-    axi_slave DUT_slave(    ACLK,
-                            ARESET,
-                            AWADDR,
-                            AWVALID,
-                            AWREADY,
-                            WDATA,
-                            WVALID,
-                            WSTRB,
-                            WREADY,
-                            BREADY,
-                            BRESP,
-                            BVALID);
-
-
+    axi_slave2 slave (
+        .ACLK(ACLK),
+        .ARESET(ARESET),
+        .AWVALID(AWVALID),
+        .AWADDR(AWADDR),
+        .AWREADY(AWREADY),
+        .WVALID(WVALID),
+        .WDATA(WDATA),
+        .WSTRB(WSTRB),
+        .WREADY(WREADY),
+        .BREADY(BREADY),
+        .BVALID(BVALID),
+        .BRESP(BRESP),
+        .ARVALID(ARVALID),
+        .ARADDR(ARADDR),
+        .ARREADY(ARREADY),
+        .RREADY(RREADY),
+        .RVALID(RVALID),
+        .RDATA(RDATA),
+        .RRESP(RRESP),
+        .valid(valid),
+        .aw_addr(aw_addr),
+        .w_data(w_data),
+        .w_strb(w_strb),
+        .read_valid(read_valid),
+        .ready()
+    );
+    
     always #5 ACLK = ~ACLK;
 
+    
     initial begin
         ACLK = 1'b1;
         ARESET = 1'b0;
         #20 ARESET = 1'b1;
-        // 첫번째 트랜젝션
+       
         @(posedge ACLK) begin
-            aw_addr = 32'd0;            //addr 0번지부터 작성하라
-            w_data  = 32'h12345678;
-            w_strb  = 4'b0001;          // lsb 1바이트 h'78 write 됨 Little Endian
-            valid   = 1'b1;
+        aw_addr = 32'd0; 
+        w_data  = 32'h12345678;
+        w_strb  = 4'b0001; 
+        valid   = 1'b1;
+        
+        @(posedge ACLK);
+        valid = 1'b0;
+        wait(BVALID); // BVALID 신호 대기
+        @(posedge ACLK);
+        if (BRESP == 2'b00) begin
+            $display("쓰기 성공: 주소 = %h, 데이터 = %h", aw_addr, w_data);
+        end else begin
+            $display("쓰기 실패: 주소 = %h, 데이터 = %h", aw_addr, w_data);
         end
-        @(posedge ACLK);
-        valid = 1'b0;
-        
-        @(posedge ready)begin
-            aw_addr = 32'd0;            
-            w_data  = 32'h0;
-            w_strb  = 4'b0000;          
-            valid   = 1'b0;
-        end  // wait for ready signal
 
-
-        // 두번째 트랜잭션
-        
         @(posedge ACLK);
-        aw_addr = 32'd1;                //addr 1번부터 
+        aw_addr = 32'd1; 
         w_data  = 32'h12345678;
-        w_strb  = 4'b0011;              // 78 56 write
+        w_strb  = 4'b0011; 
         valid   = 1'b1;
-        #1
-
+        
+        @(posedge ACLK);
+        valid = 1'b0; 
+        wait(BVALID); 
+        
+        @(posedge ACLK);
+        aw_addr = 32'd3; 
+        w_data  = 32'h12345678;
+        w_strb  = 4'b1101; 
+        valid   = 1'b1;
+        
         @(posedge ACLK);
         valid = 1'b0;
-        @(posedge ready)begin
-            aw_addr = 32'd0;            
-            w_data  = 32'h0;
-            w_strb  = 4'b0000;          
-            valid   = 1'b0;
-        end  // wait for ready signal
-        
-        //세번째 트랜잭션
+        wait(BVALID); 
         
         @(posedge ACLK);
-        aw_addr = 32'd3;                //addr 3번부터
+        aw_addr = 32'd7; 
         w_data  = 32'h12345678;
-        w_strb  = 4'b1101;              // 78 43 21 write
+        w_strb  = 4'b1111; 
         valid   = 1'b1;
 
         @(posedge ACLK);
-        valid = 1'b0;
-        @(posedge ready)begin
-            aw_addr = 32'd0;            
-            w_data  = 32'h0;
-            w_strb  = 4'b0000;          
-            valid   = 1'b0;
-        end  // wait for ready signal
-
-        // 네번째 트랜잭션
+        valid = 1'b0; 
+        wait(BVALID); 
         
-        @(posedge ACLK);
-        aw_addr = 32'd7;                //addr 7번부터
-        w_data  = 32'h12345678;
-        w_strb  = 4'b1111;              //78 56 43 21 write
-        valid   = 1'b1;
-
-        @(posedge ACLK);
-        valid = 1'b0;
-        @(posedge ready)begin
-            aw_addr = 32'd0;            
-            w_data  = 32'h0;
-            w_strb  = 4'b0000;          
-            valid   = 1'b0;
-        end  // wait for ready signal
-        
+        #100;
+        $finish;
     end
+    
 endmodule
