92c64ee41d708979e89e71b3104162a72b95bef8
leegwongho <lgwongho@gmail.com>
2024-10-06 21:03:05 +0900

    남은 파일 전송
    ㅇㅇ

---
+++ microblaze_led_on_off_app/Debug/src/helloworld.d
@@ -0,0 +1,21 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ C:/github/amba_bus/microblaze_led_on_off_hw/export/microblaze_led_on_off_hw/sw/microblaze_led_on_off_hw/standalone_domain/bspinclude/include/xil_printf.h \
+ C:/github/amba_bus/microblaze_led_on_off_hw/export/microblaze_led_on_off_hw/sw/microblaze_led_on_off_hw/standalone_domain/bspinclude/include/xil_types.h \
+ C:/github/amba_bus/microblaze_led_on_off_hw/export/microblaze_led_on_off_hw/sw/microblaze_led_on_off_hw/standalone_domain/bspinclude/include/xparameters.h \
+ C:/github/amba_bus/microblaze_led_on_off_hw/export/microblaze_led_on_off_hw/sw/microblaze_led_on_off_hw/standalone_domain/bspinclude/include/bspconfig.h \
+ C:/github/amba_bus/microblaze_led_on_off_hw/export/microblaze_led_on_off_hw/sw/microblaze_led_on_off_hw/standalone_domain/bspinclude/include/xparameters.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+C:/github/amba_bus/microblaze_led_on_off_hw/export/microblaze_led_on_off_hw/sw/microblaze_led_on_off_hw/standalone_domain/bspinclude/include/xil_printf.h:
+
+C:/github/amba_bus/microblaze_led_on_off_hw/export/microblaze_led_on_off_hw/sw/microblaze_led_on_off_hw/standalone_domain/bspinclude/include/xil_types.h:
+
+C:/github/amba_bus/microblaze_led_on_off_hw/export/microblaze_led_on_off_hw/sw/microblaze_led_on_off_hw/standalone_domain/bspinclude/include/xparameters.h:
+
+C:/github/amba_bus/microblaze_led_on_off_hw/export/microblaze_led_on_off_hw/sw/microblaze_led_on_off_hw/standalone_domain/bspinclude/include/bspconfig.h:
+
+C:/github/amba_bus/microblaze_led_on_off_hw/export/microblaze_led_on_off_hw/sw/microblaze_led_on_off_hw/standalone_domain/bspinclude/include/xparameters.h:
