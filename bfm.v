1a080f940de8c994b406188fadfcfdec3beac0f0
leegwongho <lgwongho@gmail.com>
2024-10-07 23:15:15 +0900

    수정

--- bfm.v
+++ bfm.v
@@ -229,9 +229,82 @@
 //     end
 // endmodule
 
+// module bfm(
+//     input clk,
+//     input reset,  // Low-active reset
+//     input   [31:0]  U_RDATA,
+//     input [15:0]    sw,
+//     output reg  U_WVALID,
+//     output reg  [31:0] U_AWADDR,
+//     output reg  [31:0] U_WDATA,
+//     output reg  [3:0]  U_STRB,
+//     output reg  U_RVALID,
+//     output reg  [31:0]  U_ARADDR,
+//     output reg  [3:0]   U_BLEN,
+//     output reg  [14:0]  led
+// );
+
+//     wire flag_m;
+//     assign flag_m = sw[15] ? 1 : 0;
+
+//     reg [10:0] clk_div;
+
+//     // 클럭 분주기를 리셋 시 초기화
+//     always @ (posedge clk or negedge reset) begin
+//         if (!reset)
+//             clk_div <= 0;   // Reset active 시 clk_div 초기화
+//         else
+//             clk_div <= clk_div + 1; 
+//     end
+
+//     // 쓰기 로직
+//     always @(posedge clk or negedge reset) begin
+//         if (!reset) begin
+//             U_WVALID <= 0;
+//             U_AWADDR <= 0;
+//             U_WDATA <= 0;
+//             U_STRB <= 0;
+//         end 
+//         else if (clk_div[5]) begin 
+//             if (flag_m == 1) begin
+//                 U_WDATA <= sw[14:0];   // sw[14:0] 값을 쓰기 데이터로 사용
+//                 U_AWADDR <= 0;         // 주소 0
+//                 U_STRB <= 4'b1111;     // 전체 4바이트를 모두 쓰기
+//                 U_WVALID <= 1;
+//             end
+//         end
+//         else begin
+//             U_WVALID <= 0;
+//         end
+//     end
+
+//     // 읽기 로직
+//     always @(posedge clk or negedge reset) begin
+//         if (!reset) begin
+//             U_RVALID <= 0;
+//             led <= 0;
+//             U_BLEN <= 0;
+//         end
+//         else begin
+//             U_ARADDR <= 0;        // 읽기 주소 0
+//             U_BLEN <= 4'b1111;    // 읽기 길이 설정 (4번 읽기)
+//             if (flag_m == 0) begin
+//                 if (clk_div[5]) begin
+//                     U_RVALID <= 1;        // 읽기 유효 신호 설정
+//                 end
+//                 else begin
+//                     U_RVALID <= 0; 
+//                     led <= U_RDATA[14:0];
+//                 end
+//             end
+//         end
+//     end
+
+// endmodule
+
 module bfm(
     input clk,
-    input reset,  // Low-active reset
+    input reset,  
     input   [31:0]  U_RDATA,
     input [15:0]    sw,
     output reg  U_WVALID,
@@ -250,7 +323,7 @@ module bfm(
     reg [10:0] clk_div;
 
     // 클럭 분주기를 리셋 시 초기화
-    always @ (posedge clk or negedge reset) begin
+    always @ (posedge clk) begin
         if (!reset)
             clk_div <= 0;   // Reset active 시 clk_div 초기화
         else
@@ -258,45 +331,45 @@ module bfm(
     end
 
     // 쓰기 로직
-    always @(posedge clk or negedge reset) begin
+    always @(posedge clk) begin
         if (!reset) begin
             U_WVALID <= 0;
             U_AWADDR <= 0;
             U_WDATA <= 0;
             U_STRB <= 0;
         end 
-        else if (clk_div[5]) begin 
-            if (flag_m == 1) begin
-                U_WDATA <= sw[14:0];   // sw[14:0] 값을 쓰기 데이터로 사용
-                U_AWADDR <= 0;         // 주소 0
-                U_STRB <= 4'b1111;     // 전체 4바이트를 모두 쓰기
-                U_WVALID <= 1;
-            end
-        end
         else begin
-            U_WVALID <= 0;
+            U_WDATA <= {16'b0, sw[14:0]}; // sw[14:0]를 쓰기 데이터로 사용
+            U_AWADDR <= 0;                // 주소 0
+            U_STRB <= 4'b1111;            // 전체 4바이트를 모두 쓰기
+            if (flag_m == 1) begin 
+                U_WVALID <= 1;
+                if (clk_div[1]) begin
+                    U_WVALID <= 0;
+                end
+            end
         end
     end
 
     // 읽기 로직
-    always @(posedge clk or negedge reset) begin
+    always @(posedge clk) begin
         if (!reset) begin
             U_RVALID <= 0;
-            led <= 0;
             U_BLEN <= 0;
+            led = 16'b1000_0000_0000_0000;
         end
         else begin
-            U_ARADDR <= 0;        // 읽기 주소 0
-            U_BLEN <= 4'b1111;    // 읽기 길이 설정 (4번 읽기)
+            U_ARADDR <= 0;            // 읽기 주소 0
+            U_BLEN <= 4'b1111;        // 읽기 길이 설정 (4번 읽기)
+            
             if (flag_m == 0) begin
-                if (clk_div[5]) begin
-                    U_RVALID <= 1;        // 읽기 유효 신호 설정
-                end
-                else begin
+                U_RVALID <= 1;      // 읽기 유효 신호 설정
+                if (clk_div[1]) begin
                     U_RVALID <= 0; 
                     led <= U_RDATA[14:0];
                 end
             end
         end
     end
+    
 endmodule
